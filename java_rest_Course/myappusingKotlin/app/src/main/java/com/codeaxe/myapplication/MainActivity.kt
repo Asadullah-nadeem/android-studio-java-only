c311790a878ca3762fb5b3aecfde68ba82f8be84
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-09-13 01:08:36 +0530

    update

--- java_rest_Course/myappusingKotlin/app/src/main/java/com/codeaxe/myapplication/MainActivity.kt
+++ java_rest_Course/myappusingKotlin/app/src/main/java/com/codeaxe/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.codeaxe.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    val buttonClick = findViewById<Button>(R.id.bbuttonimage)
+        buttonClick.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            val toast = Toast.makeText(applicationContext,"Welcome",Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.LEFT,200 , 200)
+            toast.show()
+            startActivity(intent)
+        }
     }
 }
---
+++ java_rest_Course/myappusingKotlin/app/src/main/java/com/codeaxe/myapplication/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.codeaxe.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
